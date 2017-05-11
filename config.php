a6d061acf124574ecdd32be16553f2af662e167d
Pasha <bihhi.iiyx@gmail.com>
2017-05-11 10:21:29 +0300

    clean opencart

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cleanoc.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cleanoc.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/cleanoc/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/cleanoc/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/cleanoc/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/cleanoc/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/cleanoc/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/cleanoc/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/cleanoc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/cleanoc/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/cleanoc/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/cleanoc/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/cleanoc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'cleanOC');
+define('DB_PASSWORD', '4444');
+define('DB_DATABASE', 'cleanoc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Чистый OpenCart 2.3.0.2 
+vqmod 2.6.1.-opencart
+multimerch_marketplace_8.7.2
